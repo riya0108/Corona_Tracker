8ce7f36bd9bbd75e381d183717e7cbc29b69f7f9
Riya Teri <70089241+riya0108@users.noreply.github.com>
2021-07-27 20:59:13 +0530

    Add files via upload

---
+++ mapdata.js
@@ -0,0 +1,276 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "https://simplemaps.com",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    "1": {
+      name: "Andaman And Nicobar",
+      color: "#ef476f",
+      zoomable: "yes"
+    },
+    "2": {
+      name: "Andhra Pradesh",
+      color: "#ffd166",
+      zoomable: "yes"
+    },
+    "3": {
+      name: "Arunachal Pradesh",
+      color: "#06d6a0",
+      zoomable: "yes"
+    },
+    "4": {
+      name: "Assam",
+      color: "#118ab2",
+      zoomable: "yes"
+    },
+    "5": {
+      name: "Bihar",
+      color: "#073b4c",
+      zoomable: "yes"
+    },
+    "6": {
+      name: "Chandigarh",
+      color: "#3a86ff",
+      zoomable: "yes"
+    },
+    "7": {
+      name: "Chhattisgarh",
+      color: "#8338ec",
+      zoomable: "yes"
+    },
+    "8": {
+      name: "Dadra And Nagar Haveli",
+      color: "#ff006e",
+      zoomable: "yes"
+    },
+    "9": {
+      name: "Daman And Diu",
+      color: "#fb5607",
+      zoomable: "yes"
+    },
+    "10": {
+      name: "Delhi",
+      color: "#ffbe0b",
+      zoomable: "yes"
+    },
+    "11": {
+      name: "Goa",
+      color: "#c7f9cc",
+      zoomable: "yes"
+    },
+    "12": {
+      name: "Gujarat",
+      color: "#38a3a5",
+      zoomable: "yes"
+    },
+    "13": {
+      name: "Haryana",
+      color: "#f94144",
+      zoomable: "yes"
+    },
+    "14": {
+      name: "Himachal Pradesh",
+      color: "#f3722c",
+      zoomable: "yes"
+    },
+    "16": {
+      name: "Jharkhand",
+      color: "#43aa8b",
+      zoomable: "yes"
+    },
+    "17": {
+      name: "Karnataka",
+      color: "#b7094c",
+      zoomable: "yes"
+    },
+    "18": {
+      name: "Kerala",
+      color: "#ffff3f",
+      zoomable: "yes"
+    },
+    "19": {
+      name: "Lakshadweep",
+      color: "#e71d36",
+      zoomable: "yes"
+    },
+    "20": {
+      name: "Madhya Pradesh",
+      color: "#2ec4b6",
+      zoomable: "yes"
+    },
+    "21": {
+      name: "Maharashtra",
+      color: "#f72585",
+      zoomable: "yes"
+    },
+    "22": {
+      name: "Manipur",
+      color: "#9ef01a",
+      zoomable: "yes"
+    },
+    "23": {
+      name: "Meghalaya",
+      color: "#0466c8",
+      zoomable: "yes"
+    },
+    "24": {
+      name: "Mizoram",
+      color: "#ee4266",
+      zoomable: "yes"
+    },
+    "25": {
+      name: "Nagaland",
+      color: "#540d6e",
+      zoomable: "yes"
+    },
+    "26": {
+      name: "Orissa",
+      color: "#f7c59f",
+      zoomable: "yes"
+    },
+    "27": {
+      name: "Puducherry",
+      color: "#4e598c",
+      zoomable: "yes"
+    },
+    "28": {
+      name: "Punjab",
+      color: "#deff0a",
+      zoomable: "yes"
+    },
+    "29": {
+      name: "Rajasthan",
+      color: "#ffaa00",
+      zoomable: "yes"
+    },
+    "30": {
+      name: "Sikkim",
+      color: "#7bdff2",
+      zoomable: "yes"
+    },
+    "31": {
+      name: "Tamil Nadu",
+      color: "#b388eb",
+      zoomable: "yes"
+    },
+    "32": {
+      name: "Tripura",
+      color: "#f8ffe5",
+      zoomable: "yes"
+    },
+    "33": {
+      name: "Uttar Pradesh",
+      color: "#1b9aaa",
+      zoomable: "yes"
+    },
+    "34": {
+      name: "Uttaranchal",
+      color: "#04a777",
+      zoomable: "yes"
+    },
+    "35": {
+      name: "West Bengal",
+      color: "#6fffe9",
+      zoomable: "yes"
+    },
+    "36": {
+      name: "Jammu And Kashmir",
+      color: "#b02e0c",
+      zoomable: "yes"
+    },
+    "37": {
+      name: "Telangana",
+      color: "#7c3626",
+      zoomable: "yes"
+    },
+    "38": {
+      name: "Ladakh",
+      color: "#f9c22e",
+      zoomable: "yes"
+    },
+    standard: {
+      color: "#cecece",
+      inactive: "yes"
+    }
+  },
+  locations: {
+    "0": {
+      lat: 18.987807,
+      lng: 72.836447,
+      name: "Mumbai"
+    }
+  },
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
